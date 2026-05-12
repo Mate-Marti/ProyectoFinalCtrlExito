2a48d1f35ecf243469ba9e3157b8d9ea634f36d8
Mateo M <mateo.martinezr@uqvirtual.edu.co>
2026-05-12 12:21:13 -0500

    Agregacion de metodos basicos a la clase Zona y un cambio a el module info (requires java.desktop)

--- src/main/java/co/edu/uniquindio/poo/proyectofinalctrlexito/model/Zona.java
+++ src/main/java/co/edu/uniquindio/poo/proyectofinalctrlexito/model/Zona.java
@@ -1,21 +1,64 @@
 package co.edu.uniquindio.poo.proyectofinalctrlexito.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Zona {
 
     private int idZona;
     private String nombre;
-    private String capacidad;
+    private int capacidad;
     private double preciobase;
+    private List<Asiento> listaAsientos;
 
-    public Zona(int idZona, String nombre, String capacidad, double preciobase) {
+    public Zona(int idZona, String nombre, int capacidad, double preciobase) {
 
         this.idZona = idZona;
         this.nombre = nombre;
         this.capacidad = capacidad;
         this.preciobase = preciobase;
+        this.listaAsientos = new ArrayList<>();
 
     }
 
+    //Metodo para crearla zona
+    public static Zona crearZona(int idZona, String nombre, int capacidad, double preciobase) {
+        return new Zona(idZona, nombre, capacidad, preciobase);
+    }
+
+    //Metodo para actualizr la zona
+    public void actualizarZona(String nombre, int capacidad, double preciobase) {
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.preciobase = preciobase;
+    }
+
+    //Metodo para eliminar la zona
+    public void eliminarZona() {
+        this.listaAsientos.remove(this.idZona);
+        this.idZona = 0;
+        this.nombre = null;
+        this.capacidad = 0;
+        this.preciobase = 0;
+    }
+
+    //Metodo para retornar en texto la info de la zona
+    public String listarZona() {
+        return "ID de la zona:" + idZona + "Nombre: " + nombre + "Capacidad: " + capacidad + "Preciobase: " + preciobase;
+    }
+
+    //Metodo para consultar la capacidad de la Zona
+    public int consultarCapacidadZona(Asiento asiento) {
+        int capacidad = 0;
+        for (Asiento asientos : listaAsientos) {
+            if (asientos.getEstado().equalsIgnoreCase("Disponible")) {
+                capacidad += 1;
+            }
+        }
+        return capacidad;
+    }
+
+    //getters y setters
     public int getIdZona() {
         return idZona;
     }
@@ -32,11 +75,11 @@ public class Zona {
         this.nombre = nombre;
     }
 
-    public String getCapacidad() {
+    public int getCapacidad() {
         return capacidad;
     }
 
-    public void setCapacidad(String capacidad) {
+    public void setCapacidad(int capacidad) {
         this.capacidad = capacidad;
     }
 
@@ -46,5 +89,9 @@ public class Zona {
 
     public void setPreciobase(double preciobase) {
         this.preciobase = preciobase;
+    }
+
+    public List<Asiento> getListaAsientos() {
+        return listaAsientos;
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module co.edu.uniquindio.poo.proyectofinalctrlexito {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.desktop;
 
     opens co.edu.uniquindio.poo.proyectofinalctrlexito to javafx.fxml;
     opens co.edu.uniquindio.poo.proyectofinalctrlexito.viewController to javafx.fxml;
