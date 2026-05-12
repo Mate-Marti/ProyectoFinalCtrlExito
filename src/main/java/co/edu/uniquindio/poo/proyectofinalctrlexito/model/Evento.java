d088939fd736f5699601e5926fa798f265ad02f4
Mateo M <mateo.martinezr@uqvirtual.edu.co>
2026-05-12 12:56:26 -0500

    Agregacion de metodos basicos a la clase Evento y la creacion de un enum para la misma.

--- src/main/java/co/edu/uniquindio/poo/proyectofinalctrlexito/model/Evento.java
+++ src/main/java/co/edu/uniquindio/poo/proyectofinalctrlexito/model/Evento.java
@@ -9,9 +9,9 @@ public class Evento {
     private String categoria;
     private String descripcion;
     private Date fecha;
-    private String estado;
+    private EstadoEvento estado;
 
-    public Evento(int idEvento, String nombre, String categoria, String descripcion, Date fecha, String estado) {
+    public Evento(int idEvento, String nombre, String categoria, String descripcion, Date fecha, EstadoEvento estado) {
 
         this.idEvento = idEvento;
         this.nombre = nombre;
@@ -22,6 +22,43 @@ public class Evento {
 
     }
 
+    //Metodo para actualizar los datos del evento
+    public void actualizareEvento(String nombre, String categoria, String descripcion, Date fecha) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+    }
+
+    //Metodo para poner en estado de publicado
+    public void publicarEvento() {
+        this.estado = EstadoEvento.PUBLICADO;
+    }
+
+    //Metodo para poner en estado de pausado
+    public void pausarEvento() {
+        this.estado = EstadoEvento.PAUSADO;
+    }
+
+    //Metodo para poner en estado de cancelado
+    public void cancelarEvento() {
+        this.estado = EstadoEvento.CANCELADO;
+    }
+
+    //metodo para validar si el evento esta disponible
+    public boolean validarEventoDisponible() {
+        return this.estado == EstadoEvento.PUBLICADO;
+    }
+
+    //metodo para obtener losdetalles del evento
+    public String obtenerDetalleEvento() {
+        return "Evento: " + nombre + " [" + categoria + "]\n" +
+                "Descripción: " + descripcion + "\n" +
+                "Fecha: " + fecha + "\n" +
+                "Estado actual: " + estado;
+    }
+
+    //getters y seters
     public int getIdEvento() {
         return idEvento;
     }
@@ -62,11 +99,11 @@ public class Evento {
         this.fecha = fecha;
     }
 
-    public String getEstado() {
+    public EstadoEvento getEstado() {
         return estado;
     }
 
-    public void setEstado(String estado) {
+    public void setEstado(EstadoEvento estado) {
         this.estado = estado;
     }
 }
