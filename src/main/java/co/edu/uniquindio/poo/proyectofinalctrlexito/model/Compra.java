64eeba26a9bd1549a52b92729a049d3479fdbfea
isabellavalenciag <isabella.valenciag@uqvirtual.edu.co>
2026-05-14 17:45:27 -0500

    Patron builder en compra

--- src/main/java/co/edu/uniquindio/poo/proyectofinalctrlexito/model/Compra.java
+++ src/main/java/co/edu/uniquindio/poo/proyectofinalctrlexito/model/Compra.java
@@ -11,27 +11,72 @@ public class Compra {
     //private List<>
     private TipoPago tipoPago;
 
-    public Compra(int idCompra,Date fechaCreacion,double total,EstadoCompra estadoCompra,TipoPago tipoPago) {
-
-        this.idCompra = idCompra;
-        this.fechaCreacion = fechaCreacion;
-        this.total = total;
-        this.estadoCompra = estadoCompra;
-        this.tipoPago = tipoPago;
-
+    private Compra(Builder builder) {
+        this.idCompra = builder.idCompra;
+        this.fechaCreacion = builder.fechaCreacion;
+        this.total = builder.total;
+        this.estadoCompra = builder.estadoCompra;
+        this.tipoPago = builder.tipoPago;
     }
-
-    //Metodo para cancalar la compra
     public void cancelarCompra() {
         this.estadoCompra = EstadoCompra.CANCELADA;
     }
 
     //Metodo para consultar la compra
     public String consultarCompra() {
-        return "Numero de compra:" + idCompra + "Total:" + total + "Estado de la Compra:" + estadoCompra + "TipoPago:" + tipoPago;
+        return "Numero de compra: " + idCompra +
+                " Total: " + total +
+                " Estado de la Compra: " + estadoCompra +
+                " TipoPago: " + tipoPago;
+    }
+
+    //
+    // Builder
+    //
+
+    public static class Builder {
+
+        private int idCompra;
+        private Date fechaCreacion;
+        private double total;
+        private EstadoCompra estadoCompra;
+        private TipoPago tipoPago;
+
+        public Builder() {
+        }
+
+        public Builder setIdCompra(int idCompra) {
+            this.idCompra = idCompra;
+            return this;
+        }
+
+        public Builder setFechaCreacion(Date fechaCreacion) {
+            this.fechaCreacion = fechaCreacion;
+            return this;
+        }
+
+        public Builder setTotal(double total) {
+            this.total = total;
+            return this;
+        }
+
+        public Builder setEstadoCompra(EstadoCompra estadoCompra) {
+            this.estadoCompra = estadoCompra;
+            return this;
+        }
+
+        public Builder setTipoPago(TipoPago tipoPago) {
+            this.tipoPago = tipoPago;
+            return this;
+        }
+
+        public Compra build() {
+            return new Compra(this);
+        }
     }
 
     //getters y setters
+
     public int getIdCompra() {
         return idCompra;
     }
