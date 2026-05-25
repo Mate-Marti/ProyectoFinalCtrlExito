801fbe6ff0498eb34a92b8dde75eab1227dd31a4
isabellavalenciag <isabella.valenciag@uqvirtual.edu.co>
2026-05-25 13:29:34 -0500

    corregir errores

--- src/main/java/co/edu/uniquindio/poo/proyectofinalctrlexito/model/ConciertoFactory.java
+++ src/main/java/co/edu/uniquindio/poo/proyectofinalctrlexito/model/ConciertoFactory.java
@@ -5,8 +5,14 @@ import java.util.Date;
 public class ConciertoFactory extends EventoFactory {
 
     @Override
-    public Evento crearEvento(int id, String nombre, String descripcion, Date fecha) {
-        return new ConciertoEvento(id, nombre, "Concierto", descripcion, fecha, EstadoEvento.BORRADOR);
+    public Evento crearEvento(int id, String nombre,
+                              String descripcion, Date fecha) {
+        return new ConciertoEvento(
+                String.valueOf(id),
+                nombre,
+                "Concierto",
+                descripcion
+        );
     }
 
 }
